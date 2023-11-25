654213ae4a8b4a12a9b511db668ae67336d211c4
Simon <simon.ciperle@gmail.com>
2023-11-25 15:29:46 +0100

    init commit

---
+++ packages/salesforce/src/RandomStringGenerator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace code\salesforce;
+
+class RandomStringGenerator
+{
+    /**
+     * Generate a random string of a specified length.
+     *
+     * @param int $length
+     * @return string
+     */
+    public function generateRandomString($length = 10)
+    {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+
+        return $randomString;
+    }
+}
